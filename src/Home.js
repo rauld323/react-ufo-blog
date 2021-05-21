ef2012258429450f0d9d2f927c56dee2b3022c57
Raul <rauldavila323@gmail.com>
2021-05-21 13:08:31 +0200

    onClick delete event has been completed. useEffect has also been added.

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './Bloglist';
 
 const Home = () => {
-	const [blogs, SetBlogs] = useState([
+	const [blogs, setBlogs] = useState([
 		{ title: 'Encouters in the 1960s', body: 'lorem ipsum...', author: 'Mausan', id: 1 },
 		{ title: 'Face on Mars!', body: 'lorem ipsum...', author: 'Bell', id: 2 },
-		{ title: 'Video Autopsy', body: 'lorem ipsum...', author: 'Lazaar', id: 3 }
+		{ title: 'Video Autopsy', body: 'lorem ipsum...', author: 'Lazaar', id: 3 },
+		{ title: 'Chupacabra', body: 'lorem ipsum...', author: 'Mausan', id: 4 },
+		{ title: 'Pyramids on the Moon', body: 'lorem ipsum...', author: 'Hoglan', id: 5 }
 	]);
+
+	const handleDelete = (id) =>{
+		const newBlogs = blogs.filter(blog => blog.id !== id );
+		setBlogs(newBlogs)
+	}
+
+	useEffect(() => {
+		console.log('useEffect worked')
+		console.log(blogs)
+	}, []);
 
 	return (
 		<div className="home">
-				{blogs.map((blog) => (
-					<div className="blog-preview" key={blog.id}>
-							<h2>{blog.title}</h2>
-							<p>Written by {blog.author}</p>
-					</div>
-				))}
+				<BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+				<BlogList blogs={blogs.filter((blog) => blog.author === "Mausan")} title="Mausan's Blogs!" slogan="*Top Contributor*"/>
 		</div>
 	 );
 }
