2339084ed8576ccba992ca72be13bbc17c9f07d1
Raul <rauldavila323@gmail.com>
2021-05-22 14:04:55 +0200

    Loading message has been created using useEffect.

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,25 @@ import BlogList from './Bloglist';
 
 const Home = () => {
 	const [blogs, setBlogs] = useState(null);
+	const [isPending, setPending] = useState(true)
 
 
 	useEffect(() => {
-		fetch('http://localhost:8000/blogs')
+		setTimeout(() => {
+			fetch('http://localhost:8000/blogs')
 			.then(response => {
-				return response.json()
-			})
-			.then(data => {
-				setBlogs(data)
-			})
-	}, []);
+			return response.json()
+		})
+		.then(data => {
+			setBlogs(data);
+			setPending(false)
+		});
+	}, 1000);
+}, []);
 
 	return (
 		<div className="home">
+			{ isPending && <div>The Truth is out...</div>}
 			{blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
 			{blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Mausan")} title="Mausan's Blogs!" slogan="*Top Contributor*" />}
 		</div>
