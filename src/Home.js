428b8893f73b9c6042b07033e6c56e2abf3d9e42
Raul <rauldavila323@gmail.com>
2021-05-22 13:56:19 +0200

    Delete button has been deleted and data is now fetched.

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,23 @@ import { useState, useEffect } from 'react';
 import BlogList from './Bloglist';
 
 const Home = () => {
-	const [blogs, setBlogs] = useState([
-		{ title: 'Encouters in the 1960s', body: 'lorem ipsum...', author: 'Mausan', id: 1 },
-		{ title: 'Face on Mars!', body: 'lorem ipsum...', author: 'Bell', id: 2 },
-		{ title: 'Video Autopsy', body: 'lorem ipsum...', author: 'Lazaar', id: 3 },
-		{ title: 'Chupacabra', body: 'lorem ipsum...', author: 'Mausan', id: 4 },
-		{ title: 'Pyramids on the Moon', body: 'lorem ipsum...', author: 'Hoglan', id: 5 }
-	]);
+	const [blogs, setBlogs] = useState(null);
 
-	const handleDelete = (id) =>{
-		const newBlogs = blogs.filter(blog => blog.id !== id );
-		setBlogs(newBlogs)
-	}
 
 	useEffect(() => {
-		console.log('useEffect worked')
-		console.log(blogs)
+		fetch('http://localhost:8000/blogs')
+			.then(response => {
+				return response.json()
+			})
+			.then(data => {
+				setBlogs(data)
+			})
 	}, []);
 
 	return (
 		<div className="home">
-				<BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-				<BlogList blogs={blogs.filter((blog) => blog.author === "Mausan")} title="Mausan's Blogs!" slogan="*Top Contributor*"/>
+			{blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
+			{blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Mausan")} title="Mausan's Blogs!" slogan="*Top Contributor*" />}
 		</div>
 	 );
 }
