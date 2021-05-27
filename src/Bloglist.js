eb9934ccfb79da79374b89b1c2edbfbb3a312e64
Raul <rauldavila323@gmail.com>
2021-05-27 10:44:28 +0200

    useFetch now aborts and blogDetail component has been created

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 const BlogList = ({ blogs, title , slogan }) => {
 
 	return (
@@ -5,8 +7,10 @@ const BlogList = ({ blogs, title , slogan }) => {
 			<h2>{title} {slogan}</h2>
 				{blogs.map((blog) => (
 				<div className="blog-preview" key={blog.id}>
-					<h2>{blog.title}</h2>
-					<p>Written by {blog.author}</p>
+					<Link to={`/blogs/${blog.id}`}>
+						<h2>{blog.title}</h2>
+						<p>Written by {blog.author}</p>
+					</Link>
 				</div>
 			))}
 		</div>
