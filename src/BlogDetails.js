2756cc4d43e8b4469ea344c1008b545655ee15c0
Raul <rauldavila323@gmail.com>
2021-05-28 11:30:59 +0200

    DEL method has been added to blog pages.

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from 'react-router-dom';
 import useFetch from './useFetch';
+import { useHistory } from 'react-router-dom';
 
 const BlogDetails = () => {
 	const { id } = useParams();
 	const { data: blog, error, isPending } = useFetch(
 		'http://localhost:8000/blogs/' + id
 	);
+	const history = useHistory();
+
+	const handleClick = () => {
+		fetch('http://localhost:8000/blogs' + blog.id, {
+			method: 'DELETE'
+		}).then(() => {
+			history.push('/');
+		});
+	};
 
 	return (
 		<div className='blog-details'>
@@ -23,6 +33,9 @@ const BlogDetails = () => {
 						{blog.author}
 					</p>
 					<div>{blog.body}</div>
+					<button onClick={handleClick}>
+						Delete Blog
+					</button>
 				</article>
 			)}
 		</div>
