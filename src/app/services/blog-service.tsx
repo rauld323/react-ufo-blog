13aba4a46c6a8e5e4151732a44e92134f47be8ed
Raul Davila <rauldavila323@gmail.com>
2023-12-07 22:03:14 +0100

    add inteface type to api call

--- src/app/services/blog-service.tsx
+++ src/app/services/blog-service.tsx
@@ -1,15 +1,23 @@
 import axios from "axios";
 
+interface Blog {
+  id: string;
+  title: string;
+  body: string;
+  author: string;
+  name: string;
+}
+
 const API_BASE_URL = "http://localhost:8000";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
 });
 
-export const fetchBlogs = async () => {
+export const fetchBlogs = async (): Promise<Blog[]> => {
   try {
     const response = await axiosInstance.get("/blogs");
-    return response.data;
+    return response.data as Blog[];
   } catch (error) {
     throw new Error("Error fetching blogs: " + error.message);
   }
