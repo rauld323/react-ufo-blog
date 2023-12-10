ea9dc40e9b0faff4e50c70faf6e3a46d30677fc3
Raul Davila <rauldavila323@gmail.com>
2023-12-10 20:30:44 +0100

    add isFetching and isError to BlogDetails

--- src/app/Blogs/[blogId]/page.tsx
+++ src/app/Blogs/[blogId]/page.tsx
@@ -4,18 +4,26 @@ import { useBlogDetails } from "../../hooks/blog-hook";
 
 export default function BlogDetails() {
   const { blogId } = useParams();
-  const { data: blogDetails } = useBlogDetails(blogId as string);
-  console.log(blogDetails);
+  const {
+    data: blogDetails,
+    isFetching,
+    isError,
+  } = useBlogDetails(blogId as string);
+
   return (
     <div>
-      <>
-        <h2>{blogDetails?.title}</h2>
-        <div className="blog-preview">
-          <h2>{blogDetails?.name}</h2>
-          <article>{blogDetails?.body}</article>
-          <p>Written by {blogDetails?.author} </p>
-        </div>
-      </>
+      {isFetching && <div>Loading...</div>}
+      {blogDetails && (
+        <>
+          <h2>{blogDetails.title}</h2>
+          <div className="blog-preview">
+            <h2>{blogDetails.name}</h2>
+            <article>{blogDetails.body}</article>
+            <p>Written by {blogDetails.author} </p>
+          </div>
+        </>
+      )}
+      {isError && <>Crap, we are having issues</>}
     </div>
   );
 }
