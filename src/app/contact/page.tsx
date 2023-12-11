0f368335952e98649574873164b2e650e938779b
Raul Davila <rauldavila323@gmail.com>
2023-12-11 09:49:40 +0100

    add Controllers to form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,29 +1,58 @@
 "use client";
 import { useForm } from "react-hook-form";
+import { Controller } from "react-hook-form";
 
 const Contact = () => {
+  interface IContactFormValue {
+    fullName: string;
+    subject: string;
+    email: string;
+    textArea: string;
+  }
+  const defaultValues = {
+    fullName: "",
+    subject: "",
+    email: "",
+    textArea: "",
+  };
+
   const {
+    control,
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<IContactFormValue>({ mode: "onChange", defaultValues });
 
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
-      <input {...register("fullName", { required: true })} />
-      {errors.fullName && <p>Name is required.</p>}
+    <div>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <Controller
+          control={control}
+          name="fullName"
+          render={({ field }) => <input {...field} />}
+        />
 
-      <input {...register("subject", { required: true })} />
-      {errors.subject && <p>Subject is required.</p>}
+        <Controller
+          control={control}
+          name="subject"
+          render={({ field }) => <input {...field} />}
+        />
 
-      <input {...register("email", { pattern: /\d+/ })} />
-      {errors.email && <p>Please enter an email</p>}
+        <Controller
+          control={control}
+          name="email"
+          render={({ field }) => <input {...field} />}
+        />
 
-      <textarea {...register("text-area", { required: true })} />
-      {errors.textArea && <p>Please enter your message here</p>}
+        <Controller
+          control={control}
+          name="textArea"
+          render={({ field }) => <textarea {...field} />}
+        />
 
-      <input type="submit" />
-    </form>
+        <input type="submit" />
+      </form>
+    </div>
   );
 };
 
