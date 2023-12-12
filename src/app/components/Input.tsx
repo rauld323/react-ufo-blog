a01ac7b45d88913d9b0c70ccd1fa9bc2cdd26bd5
Raul Davila <rauldavila323@gmail.com>
2023-12-12 10:29:30 +0100

    replace inputs in Contact form

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,24 +1,26 @@
 import React, { FC } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 interface IProps {
-  field: any;
+  field?: any;
   placeHolder?: string;
   label?: string;
-  error?: boolean;
+  error: boolean;
   errorMessage?: string;
+  type?: string;
 }
 
 const Input: FC<IProps> = ({
   placeHolder = "Raul",
   label = "Name",
-  error = false,
+  error,
   errorMessage = "Something went wrong!",
+  type = "text",
 }) => {
   return (
     <StyledInputContainer>
       <StyledLabel error={error}>{label}</StyledLabel>
-      <input placeholder={placeHolder} />
+      <StyledInput placeholder={placeHolder} type={type} />
       {error && (
         <StyledErrorMessage error={error}>{errorMessage}</StyledErrorMessage>
       )}
@@ -28,14 +30,36 @@ const Input: FC<IProps> = ({
 
 export default Input;
 
+const inputAnimationShake = keyframes`
+  20% {
+    transform: translateX(3px);
+  }
+  60% {
+    transform:  translateX(-5px);
+  }
+  20% {
+    transform:  translateX(3px);
+  }
+`;
+
 const StyledInputContainer = styled.div`
   margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledInput = styled.input`
+  &:invalid {
+    animation: ${inputAnimationShake} 0.4s;
+  }
 `;
 
 const StyledLabel = styled.label<{ error: boolean }>`
   color: ${(props) => (props.error ? "red" : "black")};
+  font-size: 13px;
 `;
 
-const StyledErrorMessage = styled.h5<{ error: boolean }>`
+const StyledErrorMessage = styled.div<{ error: boolean }>`
   color: ${(props) => (props.error ? "red" : "black")};
+  font-size: 14px;
 `;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,44 +7,68 @@ import {
   ContactFormSchema,
   contactFormSchema,
 } from "./formSchema/contactFormSchema";
+import Input from "../components/Input";
 
 const Contact = () => {
-  const { control, handleSubmit } = useForm<ContactFormSchema>({
+  const { control, handleSubmit, formState } = useForm<ContactFormSchema>({
     mode: "onChange",
     resolver: zodResolver(contactFormSchema),
   });
 
   return (
-    <StyledContainer>
-      <h1>Contact Us!</h1>
-      <StyledForm onSubmit={handleSubmit((data) => console.log(data))}>
-        <Controller
-          control={control}
-          name="fullName"
-          render={({ field }) => <input {...field} />}
-        />
+    <>
+      <StyledContainer>
+        <h1>Contact Us!</h1>
+        <StyledForm onSubmit={handleSubmit((data) => console.log(data))}>
+          <Controller
+            control={control}
+            name="fullName"
+            render={({ field }) => (
+              <Input
+                label={"Name"}
+                placeHolder={"Raul Davila"}
+                {...field}
+                error={false}
+              />
+            )}
+          />
 
-        <Controller
-          control={control}
-          name="subject"
-          render={({ field }) => <input {...field} />}
-        />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field }) => (
+              <Input
+                label={"Email"}
+                placeHolder={"your@email.com"}
+                {...field}
+                error={false}
+              />
+            )}
+          />
 
-        <Controller
-          control={control}
-          name="email"
-          render={({ field }) => <input {...field} />}
-        />
+          <Controller
+            control={control}
+            name="subject"
+            render={({ field }) => (
+              <Input
+                label={"Subject"}
+                placeHolder={"Math"}
+                {...field}
+                error={false}
+              />
+            )}
+          />
 
-        <Controller
-          control={control}
-          name="textArea"
-          render={({ field }) => <textarea {...field} />}
-        />
+          <Controller
+            control={control}
+            name="textArea"
+            render={({ field }) => <textarea {...field} />}
+          />
 
-        <input type="submit" />
-      </StyledForm>
-    </StyledContainer>
+          <input type="submit" />
+        </StyledForm>
+      </StyledContainer>
+    </>
   );
 };
 
