072d3b087f21d15722cfe81be6329cd38bbc873c
Raul Davila <rauldavila323@gmail.com>
2023-12-13 22:22:13 +0100

    add styling to blog conent

--- src/app/Blogs/[blogId]/page.tsx
+++ src/app/Blogs/[blogId]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useParams } from "next/navigation";
+import styled from "styled-components";
 import { useBlogDetails } from "../../hooks/blog-hook";
 
 export default function BlogDetails() {
@@ -15,16 +16,38 @@ export default function BlogDetails() {
       {isFetching && <div>Loading...</div>}
       {blogDetails && (
         <article>
-          <img
+          <StyledImage
             src={"http://localhost:8000/" + blogDetails.blogImage}
             alt="blogImage"
           />
-          <h2>{blogDetails.name}</h2>
-          <p>Written by {blogDetails.author}</p>
-          <div>{blogDetails.content}</div>
+          <StyledBlogText>
+            <StyledName>{blogDetails.name}</StyledName>
+            <p>Written by {blogDetails.author}</p>
+            <StyledContent>{blogDetails.content}</StyledContent>
+          </StyledBlogText>
         </article>
       )}
       {isError && <>Crap, we are having issues</>}
     </div>
   );
 }
+
+const StyledImage = styled.img`
+  margin-left: 15%;
+  margin-bottom: 10px;
+  width: 70%;
+`;
+
+const StyledName = styled.h2`
+  font-size: 20px;
+  color: #f1356d;
+  margin-bottom: 10px;
+`;
+
+const StyledContent = styled.div`
+  margin: 20px 0;
+`;
+
+const StyledBlogText = styled.div`
+  margin: 0 20px;
+`;
